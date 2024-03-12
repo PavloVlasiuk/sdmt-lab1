2f7f4fc75ae96e58f7567df55dae180e5e053757
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-03-12 23:33:20 +0200

    feat: created file manager to read and write files

---
+++ src/file-manager.ts
@@ -0,0 +1,28 @@
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'node:fs';
+
+export class FileManager {
+  static read (path: string): string {
+    if (!existsSync(path)) throw new Error('Input file path is incorrect');
+
+    const content = readFileSync(path, 'utf-8');
+
+    return content;
+  }
+
+  static write (filename: string, content: string): void {
+    const fileExtension = filename.split('.')[1];
+    const htmlExtension = 'html';
+
+    if (fileExtension !== htmlExtension) throw new Error('Invalid extension of output file');
+
+    const outputDir = './parsedFiles/';
+
+    this.createDirIfNotExists(outputDir);
+    
+    writeFileSync(outputDir + filename, content);
+  }
+
+  private static createDirIfNotExists (dir: string) {
+    if (!existsSync(dir)) mkdirSync(dir);
+  }
+}
