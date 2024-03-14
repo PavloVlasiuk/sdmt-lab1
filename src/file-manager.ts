cf4e1e0e23720d869c772eb5e5fb0d4efa79f733
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-03-14 22:54:03 +0200

    fix: added input and output file definition check

--- src/file-manager.ts
+++ src/file-manager.ts
@@ -2,6 +2,7 @@ import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'node:fs';
 
 export class FileManager {
   static read (path: string): string {
+    if (!path) throw new Error('Input file is not specified');
     if (!existsSync(path)) throw new Error('Input file path is incorrect');
 
     const content = readFileSync(path, 'utf-8');
@@ -10,6 +11,8 @@ export class FileManager {
   }
 
   static write (filename: string, content: string): void {
+    if (!filename) throw new Error('Output filename is not specified');
+
     const fileExtension = filename.split('.')[1];
     const htmlExtension = 'html';
 
