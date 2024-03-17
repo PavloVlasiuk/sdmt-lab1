827ff24dda7acdf90e92c56093aac226d2d2679e
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-03-18 01:00:28 +0200

    refactor: located constant regular expressions in separated file, created common regex for fonts marking and enhanced parseParagraph method

--- src/md-parser.ts
+++ src/md-parser.ts
@@ -1,24 +1,19 @@
+import { CORRECT_PATTERN } from './constants/regexes';
 import { MdValidator } from './md-validator';
 
 export class MdParser {
-  // create common pattern with capturing groups
-  // replace /s/S with dot
-  // add symbols : ; ! ? to the end of patterns
   private readonly fontsMarking = [
     {
-      pattern: /(^|\s)\*\*((?=\S)[\s\S]*?(?=\S))\*\*([\s.,]|$)/g,
       marking: '**',
       startTag: '<b>',
       endTag: '</b>',
     },
     {
-      pattern: /(^|\s)_((?=\S)[\s\S]*?(?=\S))_([\s.,]|$)/g,
       marking: '_',
       startTag: '<i>',
       endTag: '</i>',
     },
     {
-      pattern: /(^|\s)`((?=\S)[\s\S]*?(?=\S))`([\s.,]|$)/g,
       marking: '`',
       startTag: '<tt>',
       endTag: '</tt>',
@@ -26,28 +21,28 @@ export class MdParser {
   ];
   private readonly mdValidator = new MdValidator();
 
-  parse (text: string) {
-    const { formattedText, preformattedParts } = this.replacePreformattedText(text);
+  parse (text: string): string {
+    const { paragraphsText, preformattedParts } = this.replacePreformattedText(text);
 
-    const parsedParagraphs = this.parseParagraphs(formattedText);
+    const parsedParagraphs = this.parseParagraphs(paragraphsText);
 
     const parsedText = this.returnPreformattedText(parsedParagraphs, preformattedParts);
 
     return parsedText;
   }
 
-  private replacePreformattedText (text: string): { formattedText: string, preformattedParts: Map<number, string> } {
+  private replacePreformattedText (text: string): { paragraphsText: string, preformattedParts: Map<number, string> } {
     const preformattedParts: Map<number, string> = new Map();
 
     let index = 0;
-    const formattedText = text.replace(/(?<=\s)```\n[\s\S]*?\n```(?=\s)/g, (match) => {
+    const paragraphsText = text.replace(/(?<=\s)```\n[\s\S]*?\n```(?=\s)/g, (match) => {
       const placeholder = `<pre>${index}</pre>`;
       preformattedParts.set(index, match.slice(3, -3));
       index++;
       return placeholder;
     });
 
-    return { formattedText, preformattedParts };
+    return { paragraphsText, preformattedParts };
   }
 
   private returnPreformattedText (indexedText: string, parts: Map<number, string>): string {
@@ -75,13 +70,11 @@ export class MdParser {
     this.mdValidator.checkNesting(paragraph);
     this.mdValidator.checkIncomplete(paragraph);
 
-    for (const font of this.fontsMarking) {
-      if (!font.pattern.test(paragraph)) continue;
-
-      paragraph = paragraph.replace(font.pattern, (match, startChar, textPart, endChar) => {
-        return startChar + font.startTag + textPart + font.endTag + endChar;
-      });
-    }
+    paragraph = paragraph.replace(CORRECT_PATTERN, (match, startChar, marking, textPart, endChar) => {
+      const font = this.fontsMarking.find((f) => f.marking === marking);
+      const parsedParagraph = startChar + font?.startTag + textPart + font?.endTag + endChar;
+      return parsedParagraph;
+    });
 
     return paragraph;
   }
--- src/md-validator.ts
+++ src/md-validator.ts
@@ -1,14 +1,10 @@
+import { INCOMPLETE_PATTERNS, NESTED_PATTERNS } from './constants/regexes';
+
 export class MdValidator {
   checkNesting (text: string) {
     const errorMessage = 'Invalid markdown *nested marking*'; 
-
-    const nestedPatternRegexes = [
-      /(?:^|\s)([*]{2}|[_`])\S.*?\s([*]{2}|[_`])\S[\s\S]*?\S\2\s[\s\S]*?\S\1(?:[\s.,:;!?]|$)/,
-      /(?:^|\s)([*]{2}|[_`])([*]{2}|[_`])\S.*?\S\2\1(?:[\s.,:;!?]|$)/,
-      /(^|\s)(?<outer>[*]{2}|[_`])(?=\S).*?((\s([*]{2}|[_`])\S)|(\S([*]{2}|[_`])\s)).*?(?<=\S)\k<outer>([\s.,:;!?]|$)/,
-    ];
     
-    for (const regex of nestedPatternRegexes) {
+    for (const regex of NESTED_PATTERNS) {
       if (text.includes('<pre>')) {
         text.split(/<pre>\d+<\/pre>/).find((part) => {
           if (regex.test(part)) throw new Error(errorMessage);
@@ -22,16 +18,8 @@ export class MdValidator {
 
   checkIncomplete (text: string) {
     const errorMessage = 'Invalid markdown *incomplete marking*';
-    const incompletePatternRegexes = [
-      /(?<!\*\*\S[\s\S]*?)(?<=\S)\*\*(?=\s)/,
-      /(?<=(^|\s))\*\*(?=\S)(?![\s\S]*?\S\*\*)/,
-      /(?<!_\S[\s\S]*?)(?<=\S)_(?=\s)/,
-      /(?<=(^|\s))_(?=\S)(?![\s\S]*?\S_)/,
-      /(?<!`\S[\s\S]*?)(?<=\S)`(?=\s)/,
-      /(?<=(^|\s))`(?=\S)(?![\s\S]*?\S`)/,
-    ];
 
-    for (const regex of incompletePatternRegexes) {
+    for (const regex of INCOMPLETE_PATTERNS) {
       if (text.includes('<pre>')) {
         text.split(/<pre>\d+<\/pre>/).find((part) => {
           if (regex.test(part)) throw new Error(errorMessage);
