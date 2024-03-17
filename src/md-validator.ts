97d6f2776aa5e8a4bff1c748a0c4416b97086315
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-03-18 01:12:03 +0200

    refactor: separated common logic of validation functions into checkTextByRegexes method

--- src/md-validator.ts
+++ src/md-validator.ts
@@ -1,25 +1,20 @@
 import { INCOMPLETE_PATTERNS, NESTED_PATTERNS } from './constants/regexes';
 
 export class MdValidator {
-  checkNesting (text: string) {
+  checkNesting (text: string): void {
     const errorMessage = 'Invalid markdown *nested marking*'; 
     
-    for (const regex of NESTED_PATTERNS) {
-      if (text.includes('<pre>')) {
-        text.split(/<pre>\d+<\/pre>/).find((part) => {
-          if (regex.test(part)) throw new Error(errorMessage);
-        });
-        continue;
-      }
-
-      if (regex.test(text)) throw new Error(errorMessage);
-    }
+    this.checkTextByRegexes(text, NESTED_PATTERNS, errorMessage);
   }
 
-  checkIncomplete (text: string) {
+  checkIncomplete (text: string): void {
     const errorMessage = 'Invalid markdown *incomplete marking*';
 
-    for (const regex of INCOMPLETE_PATTERNS) {
+    this.checkTextByRegexes(text, INCOMPLETE_PATTERNS, errorMessage);
+  }
+
+  private checkTextByRegexes (text: string, regexes: RegExp[], errorMessage: string): void {
+    for (const regex of regexes) {
       if (text.includes('<pre>')) {
         text.split(/<pre>\d+<\/pre>/).find((part) => {
           if (regex.test(part)) throw new Error(errorMessage);
